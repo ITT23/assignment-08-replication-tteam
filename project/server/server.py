392efdd149dea30c16818cb731cce3ead4b7d89b
Yu_ <liu95y@gmail.com>
2023-08-06 23:46:03 +0200

    Class Server

--- project/server/server.py
+++ project/server/server.py
@@ -2,33 +2,38 @@
 
 import socket
 
-def receive_base64_data():
+class Server:
+    def __init__(self, host, port, buffer_size=1024):
+        self.host = host
+        self.port = port
+        self.buffer_size = buffer_size
+
+    def receive_base64_data(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((self.host, self.port))
+        server_socket.listen(1)
+
+        print("Waiting for connection...")
+        conn, addr = server_socket.accept()
+        print("Connected to:", addr)
+
+        base64_data = b''
+
+        while True:
+            data = conn.recv(self.buffer_size)
+            if not data:
+                break
+            base64_data += data
+
+        conn.close()
+        server_socket.close()
+
+        return base64_data
+
+if __name__ == "__main__":
     HOST = '192.168.43.236'
     PORT = 7800
 
-    buffer_size = 1024
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(1)
-
-    print("Waiting for connection...")
-    conn, addr = server_socket.accept()
-    print("Connected to:", addr)
-
-    base64_data = b''
-
-    while True:
-        data = conn.recv(buffer_size)
-        if not data:
-            break
-        base64_data += data
-
-    conn.close()
-    server_socket.close()
-
-    return base64_data
-
-if __name__ == "__main__":
-    received_base64_data = receive_base64_data()
+    base64_server = Server(HOST, PORT)
+    received_base64_data = base64_server.receive_base64_data()
     print("Received Base64 Data:", received_base64_data)

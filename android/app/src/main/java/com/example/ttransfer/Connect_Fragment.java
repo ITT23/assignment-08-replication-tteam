af82ddc398f6289e5c54010dda2423f4ac659e35
Yu_ <liu95y@gmail.com>
2023-08-09 01:36:54 +0200

    custom host&port xml & sort code

---
+++ android/app/src/main/java/com/example/ttransfer/Connect_Fragment.java
@@ -0,0 +1,113 @@
+package com.example.ttransfer;
+
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Connect_Fragment extends Fragment {
+
+    private EditText host;
+    private EditText port;
+    private Button button_confirm_connect;
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.connect_fragment, container, false);
+        sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", MainActivity.MODE_PRIVATE);
+        host = rootView.findViewById(R.id.host_edit_text);
+        host.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!isValidInput(host.getText().toString())) {
+                    host.setTextColor(Color.RED);
+                } else {
+                    host.setTextColor(getResources().getColor(R.color.black));
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+        port = rootView.findViewById(R.id.port_edit_text);
+        port.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!isValidInput(port.getText().toString())) {
+                    port.setTextColor(Color.RED);
+                } else {
+                    port.setTextColor(getResources().getColor(R.color.black));
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+        button_confirm_connect = rootView.findViewById(R.id.button_confirm_connect);
+        button_confirm_connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmInput(view);
+            }
+        });
+        String saved_host = sharedPreferences.getString("host","");
+        host.setText(saved_host);
+        String saved_port = sharedPreferences.getString("port","");
+        port.setText(saved_port);
+        return rootView;
+    }
+
+    public void confirmInput(View view) {
+        String host = this.host.getText().toString();
+        String port = this.port.getText().toString();
+
+        if (!isValidInput(host) || !isValidInput(port)) {
+            if (!isValidInput(host)) {
+                this.host.setTextColor(Color.RED);
+            } else if (!isValidInput(port)) {
+                this.port.setTextColor(Color.RED);
+            } else if (!isValidInput(host) || !isValidInput(port)) {
+                this.host.setTextColor(Color.RED);
+                this.port.setTextColor(Color.RED);
+            }
+            return;
+        }
+
+        this.host.setTextColor(getResources().getColor(R.color.black));
+        this.port.setTextColor(getResources().getColor(R.color.black));
+
+// Save the input or do other operations
+        // ...
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("host", host);
+        editor.putString("port", port);
+        editor.apply();
+
+
+        // Close the fragment
+        getParentFragmentManager().beginTransaction().remove(this).commit();
+        if (getActivity() instanceof MainActivity) {
+            MainActivity mainActivity = (MainActivity) getActivity();
+            mainActivity.set_host(host);
+            mainActivity.set_port(port);
+            mainActivity.set_button_connect_visisble();
+            mainActivity.set_button_start_visible();
+        }
+    }
+
+    private boolean isValidInput(String input) {
+        // Check if input contains only digits and dots
+        return input.matches("[0-9.]+");
+    }
+}
--- android/app/src/main/java/com/example/ttransfer/MainActivity.java
+++ android/app/src/main/java/com/example/ttransfer/MainActivity.java
@@ -7,14 +7,16 @@ package com.example.ttransfer;
 // https://square.github.io/picasso/
 // Drawable Background Image: https://giphy.com/gifs/nadrient-90s-80s-computer-l41lMAzNZfYAiyR0s
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
+import androidx.fragment.app.FragmentTransaction;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -25,6 +27,7 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -39,25 +42,30 @@ import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity{
     private static final int PIC_ID = 123;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 101;
+    private static final int STORAGE_PERMISSION_REQUEST_CODE = 103;
     private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 101;
-    private static final String HOST = "192.168.43.236";
-    private static final String PORT = "7800";
-    private static final int HTTP_PORT = 8857;
     private static final String BUTTON_START_HOME = "Take Screenshot";
     private static final String BUTTON_START_RETURN = "Return";
+    private static final int HTTP_PORT = 8857;
+    private String host = "192.168.43.236";
+    private String port = "7800";
     private ConstraintLayout layout;
     private Drawable background;
     private TextView textview;
+    private Button button_connect;
     private Button button_start;
     private Button button_save;
     private Sender sender;
     private ImageView imageView;
     private Handler handler = new Handler(Looper.getMainLooper());
+    private SharedPreferences sharedPreferences;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // init
         setContentView(R.layout.activity_main);
         sender = new Sender();
         layout = (ConstraintLayout) findViewById(R.id.layout);
@@ -65,6 +73,15 @@ public class MainActivity extends AppCompatActivity{
         imageView = (ImageView) findViewById(R.id.imageView_camera);
         textview = (TextView) findViewById(R.id.textView);
         button_start = (Button) findViewById(R.id.button_start);
+        button_save = (Button) findViewById(R.id.button_save);
+        button_connect = (Button) findViewById(R.id.button_connect);
+
+        //ask camera permission
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+        }
+
+        // buttons onclickListener
         button_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -80,7 +97,6 @@ public class MainActivity extends AppCompatActivity{
 
             }
         });
-        button_save = (Button) findViewById(R.id.button_save);
         button_save.setVisibility(imageView.INVISIBLE);
         button_save.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -88,55 +104,102 @@ public class MainActivity extends AppCompatActivity{
                 saveImageToGallery();
             }
         });
+        button_connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.add(R.id.fragment_container, new Connect_Fragment());
+                transaction.commit();
+                button_connect.setVisibility(ImageView.INVISIBLE);
+                button_start.setVisibility(ImageView.INVISIBLE);
+            }
+        });
     }
 
-    public void openCameraActivity() {
+    // start_button click event
+    private void openCameraActivity() {
         Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(camera_intent, PIC_ID);
     }
 
-    public void saveImageToGallery() {
+    // save_button click event
+    private void saveImageToGallery() {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
         } else {
-            saveImage();
-        }
-    }
-
-    private void saveImage() {
-        Bitmap imageBitmap = drawableToBitmap(imageView.getDrawable());
-        if (imageBitmap == null) {
-            Toast.makeText(this, "Image not available in ImageView.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        ContentResolver contentResolver = getContentResolver();
-        ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.DISPLAY_NAME, "ImageFromImageView");
-        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-
-        // Save the image to the gallery
-        Uri imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-        if (imageUri != null) {
-            try {
-                OutputStream outputStream = contentResolver.openOutputStream(imageUri);
-                if (outputStream != null) {
-                    imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-                    outputStream.close();
-                    Toast.makeText(this, "Image saved to gallery.", Toast.LENGTH_SHORT).show();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                Toast.makeText(this, "Failed to save image.", Toast.LENGTH_SHORT).show();
+            Bitmap imageBitmap = drawableToBitmap(imageView.getDrawable());
+            if (imageBitmap == null) {
+                Toast.makeText(this, "Image not available in ImageView.", Toast.LENGTH_SHORT).show();
+                return;
             }
-        } else {
-            Toast.makeText(this, "Failed to create image in gallery.", Toast.LENGTH_SHORT).show();
+
+            ContentResolver contentResolver = getContentResolver();
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.DISPLAY_NAME, "ImageFromImageView");
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+
+            // Save the image to the gallery
+            Uri imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            if (imageUri != null) {
+                try {
+                    OutputStream outputStream = contentResolver.openOutputStream(imageUri);
+                    if (outputStream != null) {
+                        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                        outputStream.close();
+                        Toast.makeText(this, "Image saved to gallery.", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Failed to save image.", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Failed to create image in gallery.", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
+    // comfirm event after took phote
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap photo;
+        String bitmap_to_string;
+        if (requestCode == PIC_ID) {
+            photo = (Bitmap) data.getExtras().get("data");
+            bitmap_to_string = bitmapToString(photo);
+            //Log.i("BitmapString",bitmap_to_string);
+            sender.execute(bitmap_to_string, host, port);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    getImageFromHTTP();
+                    hide_homepage();
+                }
+            }, 300);
+        }
+    }
+
+    // change "pages" function 1
+    private void hide_homepage() {
+        textview.setVisibility(View.INVISIBLE);
+        background.setAlpha(0);
+        button_save.setVisibility(imageView.VISIBLE);
+        button_start.setText(BUTTON_START_RETURN);
+        button_connect.setVisibility(imageView.INVISIBLE);
+    }
+
+    // change "pages" function 2
+    private void return_to_homepage() {
+        textview.setVisibility(View.VISIBLE);
+        background.setAlpha(255);
+        button_save.setVisibility(imageView.INVISIBLE);
+        button_start.setText(BUTTON_START_HOME);
+        button_connect.setVisibility(imageView.VISIBLE);
+    }
+
+    // a function used for saveImageToGallery()
     private Bitmap drawableToBitmap(Drawable drawable) {
         if (drawable instanceof BitmapDrawable) {
             return ((BitmapDrawable) drawable).getBitmap();
@@ -153,39 +216,6 @@ public class MainActivity extends AppCompatActivity{
         }
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Bitmap photo;
-        String bitmap_to_string;
-        if (requestCode == PIC_ID) {
-            photo = (Bitmap) data.getExtras().get("data");
-            bitmap_to_string = bitmapToString(photo);
-            //Log.i("BitmapString",bitmap_to_string);
-            sender.execute(bitmap_to_string, HOST, PORT);
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    getImageFromHTTP();
-                    hide_homepage();
-                }
-            }, 300);
-        }
-    }
-
-    private void hide_homepage() {
-        textview.setVisibility(View.INVISIBLE);
-        background.setAlpha(0);
-        button_save.setVisibility(imageView.VISIBLE);
-        button_start.setText(BUTTON_START_RETURN);
-    }
-
-    private void return_to_homepage() {
-        textview.setVisibility(View.VISIBLE);
-        background.setAlpha(255);
-        button_save.setVisibility(imageView.INVISIBLE);
-        button_start.setText(BUTTON_START_HOME);
-    }
-
     private String bitmapToString(Bitmap bitmap) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
@@ -194,7 +224,44 @@ public class MainActivity extends AppCompatActivity{
     }
 
     private void getImageFromHTTP() {
-        String http = "http://"+ HOST + ":" + HTTP_PORT + "/screenshot/screenshot.png";
+        String http = "http://"+ host + ":" + HTTP_PORT + "/screenshot/screenshot.png";
         Picasso.get().load(http).into(imageView);
+    }
+
+    public void set_button_connect_visisble() {
+        button_connect.setVisibility(ImageView.VISIBLE);
+    }
+
+    public void set_button_start_visible() {
+        button_start.setVisibility(ImageView.VISIBLE);
+    }
+
+    public void set_host(String h) {
+        this.host = h;
+    }
+
+    public void set_port(String p) {
+        this.port = p;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted ;)
+            } else {
+                // Permission denied :(
+            }
+        }
+
+        if (requestCode == STORAGE_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted ;)
+            } else {
+                // Permission denied :(
+            }
+        }
     }
 }
